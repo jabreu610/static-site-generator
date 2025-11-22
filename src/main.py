d987b769aba0160ee5b9da8c1ca255a4bf2d92c6
Jordan Abreu <jabreu610@gmail.com>
2025-11-22 00:27:32 -0500

    Implement public directory initialization function

--- src/main.py
+++ src/main.py
@@ -1,9 +1,32 @@
+import os
+import shutil
+
 from textnode import TextNode, TextType
+
+DEST = "./public"
+SRC = "./static"
+
+
+def initialize_public(path=""):
+    global DEST, SRC
+    destination = os.path.join(DEST, path)
+    source = os.path.join(SRC, path)
+    if os.path.exists(destination):
+        shutil.rmtree(destination)
+    os.mkdir(destination)
+    for entry in os.listdir(source):
+        to_copy = os.path.join(source, entry)
+        if os.path.isdir(to_copy):
+            initialize_public(os.path.join(path, entry))
+        else:
+            shutil.copy(
+                to_copy,
+                destination,
+            )
 
 
 def main():
-    node = TextNode("This is anchor text", TextType.LINK, "https://www.example.com")
-    print(node)
+    initialize_public()
 
 
 main()
